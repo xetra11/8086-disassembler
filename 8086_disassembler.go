f4d993bfe835b0573203e071a496f75668088d1e
xetra11 <xetra11@outlook.com>
2025-12-29 18:53:35 +0100

    feat(dis): add testing logic to see if bits are stored in vars properly

--- 8086_disassembler.go
+++ 8086_disassembler.go
@@ -12,15 +12,51 @@ func main() {
 	b := make([]byte, 2)
 	_, error = file.Read(b)
 	check(error)
-	for j := 0; j < 2; j++ {
-		for i := 0; i < 8; i++ {
-			fmt.Printf("%d", (b[j]>>i)&1)
-			if (i == 7) {
-				fmt.Print(" ")
-			}
-		}
-	}
 
+	b7 := (b[0] & 0b10000000) >> 7
+	b6 := (b[0] & 0b01000000) >> 6
+	b5 := (b[0] & 0b00100000) >> 5
+	b4 := (b[0] & 0b00010000) >> 4
+	b3 := (b[0] & 0b00001000) >> 3
+	b2 := (b[0] & 0b00000100) >> 2
+	b1 := (b[0] & 0b00000010) >> 1
+	b0 := (b[0] & 0b00000001) >> 0
+
+	b15 := (b[1] & 0b10000000) >> 7
+	b14 := (b[1] & 0b01000000) >> 6
+	b13 := (b[1] & 0b00100000) >> 5
+	b12 := (b[1] & 0b00010000) >> 4
+	b11 := (b[1] & 0b00001000) >> 3
+	b10 := (b[1] & 0b00000100) >> 2
+	b09 := (b[1] & 0b00000010) >> 1
+	b08 := (b[1] & 0b00000001) >> 0
+
+	fmt.Printf("%b", b7)
+	fmt.Printf("%b", b6)
+	fmt.Printf("%b", b5)
+	fmt.Printf("%b", b4)
+	fmt.Printf("%b", b3)
+	fmt.Printf("%b", b2)
+	fmt.Printf("%b", b1)
+	fmt.Printf("%b", b0)
+
+	fmt.Print(" ")
+
+	fmt.Printf("%b", b15)
+	fmt.Printf("%b", b14)
+	fmt.Printf("%b", b13)
+	fmt.Printf("%b", b12)
+	fmt.Printf("%b", b11)
+	fmt.Printf("%b", b10)
+	fmt.Printf("%b", b09)
+	fmt.Printf("%b", b08)
+
+	///////////////////////////////////////////
+	// for _, v := range b {		 //
+	// 	fmt.Printf("%08b ", v)		 //
+	// }					 //
+	// fmt.Println()			 //
+	///////////////////////////////////////////
 }
 
 func check(error error) {
